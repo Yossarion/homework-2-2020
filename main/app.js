f2e10719fe931ec8677437bf580660a2b5ae8d60
Yossarion <dinu.ion2606@gmail.com>
2020-11-25 12:18:52 +0200

    solved the homework lol

--- main/app.js
+++ main/app.js
@@ -1,5 +1,23 @@
 function addTokens(input, tokens){
+    if(typeof input !== "string") {
+    throw new Error ('Invalid input')}
+    //According to the TODO-EN.md file the error should be 'Input should be a string' but the test checks for 'Invalid input'
+    if(input.length < 6) {
+    throw new Error ('Input should have at least 6 characters')
+    }
+    for (let i = 0; i < tokens.length ; i++){
+        if (typeof tokens[i].tokenName !== "string")
+            throw new Error ('Invalid array format')
+    }
+
+    for (let i = 0; i < tokens.length ; i++){
+        input = input.replace(`...`, '${'+tokens[i].tokenName+'}')
+       
+    }  
+    //I find it strange that the dots should be replaced with the token name + the dollar sign and the parenthesis, but did it anywas
     
+    return input
+
 }
 
 const app = {
